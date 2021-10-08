02c7c7e7429f3e58006aff8f1cf924d7a6e342db
Jeff Lee <jeffjwl@gmail.com>
2021-10-07 19:34:32 -0500

    add firebase db

---
+++ src/utilities/firebase.js
@@ -0,0 +1,71 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+import { useState, useEffect } from "react";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpfpkvbw08USWHsCs6sOsUv2r8WUH35dw",
+
+  authDomain: "cs497-scheduler-f7070.firebaseapp.com",
+
+  databaseURL: "https://cs497-scheduler-f7070-default-rtdb.firebaseio.com",
+
+  projectId: "cs497-scheduler-f7070",
+
+  storageBucket: "cs497-scheduler-f7070.appspot.com",
+
+  messagingSenderId: "691335432997",
+
+  appId: "1:691335432997:web:42bd5cbb47929848e228cc",
+
+  measurementId: "G-YENGMY0MZY",
+};
+
+// Initialize Firebase
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const dbRef = ref(database, path);
+    const devMode =
+      !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+    if (devMode) {
+      console.log(`loading ${path}`);
+    }
+    return onValue(
+      dbRef,
+      (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) {
+          console.log(val);
+        }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      },
+      (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      }
+    );
+  }, [path, transform]);
+
+  return [data, loading, error];
+};
